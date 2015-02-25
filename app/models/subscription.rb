798533b4e9e1f7f69a2f55e90d3d68e5abc4e7cb
Chris Scott <cyberstrike@gmail.com>
2015-02-24 23:36:37 -0500

    fixed error on Subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,12 +2,10 @@ class Subscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :plan
 
-  # accepts_nested_attributes_for :user, reject_if: :not_valid_user?
-
   attr_accessor :stripe_card_token
   attr_accessor :stripe_plan_id
 
-  validates :stripe_customer_token, :presence => true, :message => 'Stripe Error'
+  validates_presence_of :stripe_customer_token, :message => 'Stripe Error'
 
   def save_with_stripe!(stripe_plan = (stripe_plan_id.present? ? stripe_plan_id : false ) || plan_id )
     # It has dynamic stripe plan variable if it exists either set
@@ -41,19 +39,14 @@ class Subscription < ActiveRecord::Base
 
     end
 
-    # rescue Stripe::InvalidRequestError => e
-    #   logger.error "Stripe error while creating customer: #{e.message}"
-    #   error = 'There was a problem with your credit card.'
-    #   return false
-
   end
 
   def unsubscribe_with_stripe
     customer = Stripe::Customer.retrieve(stripe_customer_token)
     sub_id = customer.subscriptions.data.find{|sub| sub[:plan][:id] == plan_id.to_s}[:id]
     delete = customer.subscriptions.retrieve(sub_id).delete
-    if delete[:status] == 'canceled'
-      self.update(status: 'cancelled') # Canceled has one 'l' but sticking to established values
+    if delete[:status] == 'cancelled'
+      self.update(status: 'cancelled')
     end
 
   end
