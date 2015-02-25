fd0d9490aa604333466f119f89d47b797b70799c
DANIEL ARZUAGA <biznobo@gmail.com>
2015-02-25 01:23:22 -0500

    added crawls per day to the plan model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225055431) do
+ActiveRecord::Schema.define(version: 20150225062243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -132,6 +132,7 @@ ActiveRecord::Schema.define(version: 20150225055431) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.float    "price"
+    t.integer  "crawls_per_day"
   end
 
   create_table "process_links_batches", force: :cascade do |t|
