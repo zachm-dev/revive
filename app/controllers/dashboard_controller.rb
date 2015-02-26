f3852464ba266025feb7ee3b9daf9c7fbc0a1a18
Chris Scott <cyberstrike@gmail.com>
2015-02-25 19:26:44 -0500

    flash errors

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   before_filter :authorize
-  
+
   def index
     @dashboard = current_user.user_dashboard
     @domains = Page.find(@dashboard.top_domains)
