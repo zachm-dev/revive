ce2326c7223d4480f6fac6dd6f166c3e2d896312
Chris Scott <cyberstrike@gmail.com>
2015-02-26 04:47:36 -0500

    accounts page

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  get :account, to: 'users#account'
 
   # Sites
 
