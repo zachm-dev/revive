dbe16943690ff91d415b8d1f64b3f352c308a6a0
DANIEL ARZUAGA <biznobo@gmail.com>
2015-03-02 10:37:38 -0500

    added back sidekiq pooling

--- app/workers/process_links.rb
+++ app/workers/process_links.rb
@@ -20,10 +20,10 @@ class ProcessLinks
       internal = l.include?("#{domain}") ? true : false
       if internal == true
         if "#{response.code}" == '404'
-          Page.delay.create(status_code: "#{response.code}", url: "#{l}", internal: internal, site_id: site_id, found_on: "#{found_on}")
+          Page.create(status_code: "#{response.code}", url: "#{l}", internal: internal, site_id: site_id, found_on: "#{found_on}")
         end
       elsif internal == false
-        Page.delay.create(status_code: "#{response.code}", url: "#{l}", internal: internal, site_id: site_id, found_on: "#{found_on}")
+        Page.create(status_code: "#{response.code}", url: "#{l}", internal: internal, site_id: site_id, found_on: "#{found_on}")
       end
     end
     begin
