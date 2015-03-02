011a8640f3a371a866e8f35b7690ca151e490cc7
DANIEL ARZUAGA <biznobo@gmail.com>
2015-03-02 10:37:38 -0500

    added back sidekiq pooling

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,7 @@
-# rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
-# rails_env = Rails.env || 'development'
-#
-# redis_config = YAML.load_file(rails_root.to_s + '/config/redis.yml')
-# redis_config.merge! redis_config.fetch(Rails.env, {})
-# redis_config.symbolize_keys!
-#
-# Sidekiq.configure_server do |config|
-#   config.redis = { :url => "redis://#{redis_config[:host]}:#{redis_config[:port]}/12", :namespace => 'Sidekiq' }
-# end
-#
-# Sidekiq.configure_client do |config|
-#   config.redis = { :url => "redis://#{redis_config[:host]}:#{redis_config[:port]}/12", :namespace => 'Sidekiq' }
-# end
+Sidekiq.configure_server do |config|
+  database_url = ENV['DATABASE_URL']
+  if database_url
+    ENV['DATABASE_URL'] = "#{database_url}?pool=1"
+    ActiveRecord::Base.establish_connection
+  end
+end
