d5a9d9f6ba333cce36d317a94ab41947f606a268
DANIEL ARZUAGA <biznobo@gmail.com>
2015-03-03 14:41:39 -0500

    process links refactor

--- app/workers/verify_namecheap.rb
+++ app/workers/verify_namecheap.rb
@@ -14,7 +14,7 @@ class VerifyNamecheap
         if !url.domain.empty? && !url.public_suffix.empty?
           puts "here is the parsed url #{page.url}"
           parsed_url = url.domain + "." + url.public_suffix
-          # unless page.site.pages.where("simple_url IS NOT NULL").map(&:simple_url).include?(parsed_url)
+          unless page.site.pages.where("simple_url IS NOT NULL").map(&:simple_url).include?(parsed_url)
             puts "checking url #{parsed_url} on namecheap"
             uri = URI.parse("https://nametoolkit-name-toolkit.p.mashape.com/beta/whois/#{parsed_url}")
             http = Net::HTTP.new(uri.host, uri.port)
@@ -33,7 +33,7 @@ class VerifyNamecheap
               MajesticStats.perform_async(page.id)
               crawl.update(total_expired: craw.total_expired.to_i+1)
             end
-          # end
+          end
           
         end
       end
