f2298405f85f6e00fb3119d90dc0a0526071f0d7
DANIEL ARZUAGA <biznobo@gmail.com>
2015-03-11 17:56:34 -0400

    t

--- app/workers/process_links.rb
+++ app/workers/process_links.rb
@@ -20,14 +20,14 @@ class ProcessLinks
       internal = l.include?("#{domain}") ? true : false
       if internal == true
         if "#{response.code}" == '404'
-          Octopus.using(:slave_two) do
-            Page.delay.create(status_code: "#{response.code}", url: "#{l}", internal: internal, site_id: site_id, found_on: "#{found_on}")
-          end
+
+          Page.using(:main_shard).create(status_code: "#{response.code}", url: "#{l}", internal: internal, site_id: site_id, found_on: "#{found_on}")
+
         end
       elsif internal == false
-        Octopus.using(:main_shard) do
-          Page.delay.create(status_code: "#{response.code}", url: "#{l}", internal: internal, site_id: site_id, found_on: "#{found_on}")
-        end
+
+        Page.using(:main_shard).create(status_code: "#{response.code}", url: "#{l}", internal: internal, site_id: site_id, found_on: "#{found_on}")
+
       end
     end
     begin
