72ef0f56949d7f7e6aae884bba154b8b297b5eda
DANIEL ARZUAGA <biznobo@gmail.com>
2015-03-12 07:54:07 -0400

    t

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 6)
+workers Integer(ENV['WEB_CONCURRENCY'] || 50)
 threads_count = Integer(ENV['MAX_THREADS'] || 100)
 threads threads_count, threads_count
 
