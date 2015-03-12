7b600196efc9a09801159e6554f68df702fae711
DANIEL ARZUAGA <biznobo@gmail.com>
2015-03-12 08:31:39 -0400

    t

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-web: bundle exec passenger start -p $PORT --max-pool-size 5
+web: bundle exec passenger start -p $PORT --max-pool-size 10
 worker: bundle exec sidekiq
 processlinks: bundle exec sidekiq -q process_links
 verifydomains: bundle exec sidekiq -q verify_domains
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,9 @@ Rails.application.configure do
 
   # Generate digests for assets URLs.
   config.assets.digest = true
-
+  
+  
+  config.threadsafe!
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.
