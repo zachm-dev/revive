66c8fb2a9eae012938edfc55d00c71ebe8e3edc3
DANIEL ARZUAGA <biznobo@gmail.com>
2015-03-12 09:57:04 -0400

    t

--- app/workers/save_sites_from_google.rb
+++ app/workers/save_sites_from_google.rb
@@ -4,7 +4,7 @@ class SaveSitesFromGoogle
   sidekiq_options retry: false
   
   def perform(crawl_id, options = {})
-    crawl = Crawl.find(crawl_id)
+    crawl = Crawl.using(:main_shard).find(crawl_id)
     if !options['google_param'].nil?
       if !crawl.crawl_start_date.nil? && !crawl.crawl_end_date.nil?
         uri = URI.parse(URI.encode("https://www.google.com/search?num=10&rlz=1C5CHFA_enUS561US561&es_sm=119&q=#{crawl.base_keyword}+#{options['google_param']}&spell=1&sa=X&ei=mx7SVKn0IoboUtrdgsAL&ved=0CBwQvwUoAA&biw=1280&bih=701"))
@@ -25,18 +25,18 @@ class SaveSitesFromGoogle
       end
     end
     urls_array.map do |u|
-      Site.create(base_url: u.to_s, maxpages: crawl.maxpages.to_i, crawl_id: crawl_id)
+      Site.using(:main_shard).create(base_url: u.to_s, maxpages: crawl.maxpages.to_i, crawl_id: crawl_id)
     end
   end
   
   def on_complete(status, options)
     puts 'finished saving sites from google'
-    crawl = Crawl.find(options['crawl_id'])
+    crawl = Crawl.using(:main_shard).find(options['crawl_id'])
     Site.save_url_domains(crawl_id: options['crawl_id'])
     Site.save_moz_data(crawl_id: options['crawl_id'])
     # Site.save_majestic_data(crawl_id: options['crawl_id'])
     ids = Site.in_the_top_x_percent(20, options['crawl_id'])
-    sites = Site.find(ids)
+    sites = Site.using(:main_shard).find(ids)
     sites.each do |site|
       site.update(processing_status: "pending")
       site.create_gather_links_batch(status: "pending")
