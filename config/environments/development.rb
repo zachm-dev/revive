0b7146268d916a4372b7e37da97911f54cca6825
DANIEL ARZUAGA <biznobo@gmail.com>
2015-03-14 13:11:37 -0400

    t

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,8 +12,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.cache_store = :redis_store, { :host => "localhost",
-+                                      :port => 6379 }
+  config.cache_store = :redis_store, { :host => "localhost", :port => 6379 }
 
   # ActionMailer settings
    config.action_mailer.perform_deliveries = true
