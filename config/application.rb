29a2053c00f7e000daa8649a4d1a5aceb30d3c15
DANIEL ARZUAGA <biznobo@gmail.com>
2015-03-14 13:08:54 -0400

    added redis store env intnitialization

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 
 module Crawler
   class Application < Rails::Application
-    config.cache_store = :redis_store
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,8 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  config.cache_store = :redis_store, { :host => "localhost",
++                                      :port => 6379 }
 
   # ActionMailer settings
    config.action_mailer.perform_deliveries = true
