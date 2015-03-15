15c8035e5c0d930d6befc62ed56c5af8b66478fd
DANIEL ARZUAGA <biznobo@gmail.com>
2015-03-15 08:10:10 -0400

    t

--- app/workers/process_links.rb
+++ app/workers/process_links.rb
@@ -36,20 +36,20 @@ class ProcessLinks
     total_crawl_running = Rails.cache.decrement(["crawl/#{options['crawl_id']}/processing_batches/running"])
     total_crawl_finished = Rails.cache.increment(["crawl/#{options['crawl_id']}/processing_batches/finished"])
     
-    total_crawl_urls = Rails.cache.read(["total_crawl_urls"], raw: true).to_i
-    total_site_urls = Rails.cache.read(["site/#{options['site_id']}/total_site_urls"], raw: true).to_i
+    # total_crawl_urls = Rails.cache.read(["total_crawl_urls"], raw: true).to_i
+    # total_site_urls = Rails.cache.read(["site/#{options['site_id']}/total_site_urls"], raw: true).to_i
     
     ids = Rails.cache.read(["crawl/#{options['crawl_id']}/processing_batches/ids"])
     Rails.cache.write(["crawl/#{options['crawl_id']}/processing_batches/ids"], ids-[options['link_id']])
     
-    # if total_crawl_count == total_crawl_finished
-    #   puts 'shut down app and update crawl stats and user stats'
+    if total_crawl_count == total_crawl_finished
+      puts 'shut down app and update crawl stats and user stats'
     # elsif total_site_count == total_site_finished
     #   Site.using(:main_shard).update(options['site_id'], processing_status: 'finished', total_urls_found: total_site_urls)
     #   Crawl.using(:main_shard).update(options['crawl_id'], total_urls_found: total_crawl_urls)
-    # else
-    #   puts 'do something else'
-    # end
+    else
+      puts 'do something else'
+    end
 
     # total_time = Time.now - batch.started_at
     # pages = Page.where(site_id: site.id).using(:master)
