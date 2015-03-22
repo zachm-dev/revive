e5fe7546ccc1754339b2939d7df25beeb614fbf9
DANIEL ARZUAGA <biznobo@gmail.com>
2015-03-22 02:51:54 -0400

    check crawl stats if there is less than 100 jobs enqueued

--- app/workers/sidekiq_stats.rb
+++ app/workers/sidekiq_stats.rb
@@ -7,62 +7,66 @@ class SidekiqStats
     puts 'getting sidekiq stats'
     
     SidekiqStats.delay.start('crawl_id' => crawl_id)
-    current_stats = Rails.cache.read(["crawl/#{crawl_id}/processing_batches/running"], raw: true).to_i
     
-    if current_stats != 0
-      last_checked_time = Rails.cache.read(["stats/#{crawl_id}/last_checked_stats/time"])
+    if Sidekiq::Stats.new.enqueued < 100
       
-      if !last_checked_time.nil?
+      current_stats = Rails.cache.read(["crawl/#{crawl_id}/processing_batches/running"], raw: true).to_i
+      
+      if current_stats != 0
+        last_checked_time = Rails.cache.read(["stats/#{crawl_id}/last_checked_stats/time"])
+      
+        if !last_checked_time.nil?
         
-        last_checked_stats = Rails.cache.read(["stats/#{crawl_id}/last_checked_stats/running"], raw: true).to_i
-        time_diff = (Time.now - Rails.cache.read(["stats/#{crawl_id}/last_checked_stats/time"]).to_time)
+          last_checked_stats = Rails.cache.read(["stats/#{crawl_id}/last_checked_stats/running"], raw: true).to_i
+          time_diff = (Time.now - Rails.cache.read(["stats/#{crawl_id}/last_checked_stats/time"]).to_time)
         
-        if time_diff > 60
-          if last_checked_stats == current_stats
-            stats_verify_count = Rails.cache.read(["stats/#{crawl_id}/verify_count"], raw: true).to_i
-            if stats_verify_count == 0
-              puts 'stats have been the same for the past minute: increasing verify count'
-              Rails.cache.increment(["stats/#{crawl_id}/verify_count"])
-            elsif stats_verify_count == 1
-              puts 'stats have been the same for the past two minutes: restarting app: increasing verify count'
-              Rails.cache.increment(["stats/#{crawl_id}/verify_count"])
+          if time_diff > 60
+            if last_checked_stats == current_stats
+              stats_verify_count = Rails.cache.read(["stats/#{crawl_id}/verify_count"], raw: true).to_i
+              if stats_verify_count == 0
+                puts 'stats have been the same for the past minute: increasing verify count'
+                Rails.cache.increment(["stats/#{crawl_id}/verify_count"])
+              elsif stats_verify_count == 1
+                puts 'stats have been the same for the past two minutes: restarting app: increasing verify count'
+                Rails.cache.increment(["stats/#{crawl_id}/verify_count"])
               
-              app = HerokuApp.using(:processor).where(crawl_id: crawl_id).first
-              app_name = app.name
+                app = HerokuApp.using(:processor).where(crawl_id: crawl_id).first
+                app_name = app.name
               
-              heroku = HerokuPlatform.new
-              heroku.restart_app(app_name)
-            elsif stats_verify_count == 2
-              puts 'stats have been the same for the past three minutes: increasing verify count'
-              Rails.cache.increment(["stats/#{crawl_id}/verify_count"])
-            elsif stats_verify_count >= 3
-              puts 'app has stalled shutting it down'
-              app = HerokuApp.using(:processor).where(crawl_id: crawl_id).first
-              app_name = app.name
-              crawl = app.crawl
+                heroku = HerokuPlatform.new
+                heroku.restart_app(app_name)
+              elsif stats_verify_count == 2
+                puts 'stats have been the same for the past three minutes: increasing verify count'
+                Rails.cache.increment(["stats/#{crawl_id}/verify_count"])
+              elsif stats_verify_count >= 3
+                puts 'app has stalled shutting it down'
+                app = HerokuApp.using(:processor).where(crawl_id: crawl_id).first
+                app_name = app.name
+                crawl = app.crawl
               
-              puts 'updating crawl stats before shutting down'
-              urls_found = "crawl/#{crawl.id}/urls_found"
-              expired_domains = "crawl/#{crawl.id}/expired_domains"
-              broken_domains = "crawl/#{crawl.id}/broken_domains"
-              stats = Rails.cache.read_multi(urls_found, expired_domains, broken_domains, raw: true)
-              Crawl.using(:processor).update(crawl.id, status: 'finished', total_urls_found: stats[urls_found].to_i, total_broken: stats[broken_domains].to_i, total_expired: stats[expired_domains].to_i, msg: 'app stalled')
+                puts 'updating crawl stats before shutting down'
+                urls_found = "crawl/#{crawl.id}/urls_found"
+                expired_domains = "crawl/#{crawl.id}/expired_domains"
+                broken_domains = "crawl/#{crawl.id}/broken_domains"
+                stats = Rails.cache.read_multi(urls_found, expired_domains, broken_domains, raw: true)
+                Crawl.using(:processor).update(crawl.id, status: 'finished', total_urls_found: stats[urls_found].to_i, total_broken: stats[broken_domains].to_i, total_expired: stats[expired_domains].to_i, msg: 'app stalled')
               
-              heroku = HerokuPlatform.new
-              heroku.delete_app(app_name)
+                heroku = HerokuPlatform.new
+                heroku.delete_app(app_name)
+              end
+            else
+              puts 'resetting verify count back to 0'
+              Rails.cache.write(["stats/#{crawl_id}/verify_count"], 0, raw: true)
             end
-          else
-            puts 'resetting verify count back to 0'
-            Rails.cache.write(["stats/#{crawl_id}/verify_count"], 0, raw: true)
+            Rails.cache.write(["stats/#{crawl_id}/last_checked_stats/running"], current_stats, raw: true).to_i
           end
-          Rails.cache.write(["stats/#{crawl_id}/last_checked_stats/running"], current_stats, raw: true).to_i
-        end
       
+        end
+        Rails.cache.write(["stats/#{crawl_id}/last_checked_stats/time"], "#{Time.now}")
       end
-      Rails.cache.write(["stats/#{crawl_id}/last_checked_stats/time"], "#{Time.now}")
+      
     end
-    
-    
+
     # stats = Sidekiq::Stats.new
     # if stats.enqueued < 100
     #   sidekiq_stats = SidekiqStat.where(heroku_app_id: heroku_app_id)
