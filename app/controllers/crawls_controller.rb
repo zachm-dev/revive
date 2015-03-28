7ba9dadfff7fe611f6ee6688f8f0b97a1f157a04
DANIEL ARZUAGA <biznobo@gmail.com>
2015-03-28 14:49:37 -0400

    t

--- app/controllers/crawls_controller.rb
+++ app/controllers/crawls_controller.rb
@@ -24,7 +24,7 @@ class CrawlsController < ApplicationController
       expired_domains = "crawl/#{@project.id}/expired_domains"
       broken_domains = "crawl/#{@project.id}/broken_domains"
       progress = "crawl/#{@project.id}/progress"
-      stats = redis.read_multi(urls_found, expired_domains, broken_domains, progress, start_time, raw: true)
+      stats = redis.read_multi(urls_found, expired_domains, broken_domains, progress, raw: true)
       @urls_found = stats[urls_found].to_i
       @broken_domains = stats[broken_domains].to_i
       @expired_domains = stats[expired_domains].to_i
