45113b97e651c9bd3e78fbbf27993ab6c2866c5f
DANIEL ARZUAGA <biznobo@gmail.com>
2015-03-29 03:27:49 -0400

    added shard info model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150329070656) do
+ActiveRecord::Schema.define(version: 20150329072718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -182,6 +182,15 @@ ActiveRecord::Schema.define(version: 20150329070656) do
     t.datetime "updated_at",       null: false
     t.integer  "link_id"
     t.integer  "crawl_id"
+  end
+
+  create_table "shard_infos", force: :cascade do |t|
+    t.integer  "crawl_id"
+    t.string   "processor_name"
+    t.string   "db_url"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "sidekiq_jobs", force: :cascade do |t|
