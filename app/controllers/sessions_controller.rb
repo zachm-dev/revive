00b548b37235b7c6320746882f48b09858cac3c3
DANIEL ARZUAGA <biznobo@gmail.com>
2015-03-31 08:40:57 -0400

    t

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by_email(params[:email])
+    user = User.using(:main_shard).find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       # redirect_to :back, notice: "Logged In"
