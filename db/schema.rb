dcbe197e791acfc2f01dfc84dba80785eb37fb95
DANIEL ARZUAGA <biznobo@gmail.com>
2015-04-07 05:49:14 -0400

    added total crawl minutes to crawls model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150401064414) do
+ActiveRecord::Schema.define(version: 20150407091423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20150401064414) do
     t.string   "db_url"
     t.integer  "processor_id"
     t.string   "processor_name"
+    t.integer  "total_minutes"
   end
 
   create_table "expired_links", force: :cascade do |t|
