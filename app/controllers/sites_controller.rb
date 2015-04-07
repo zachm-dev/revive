4d79084c714b9d1d5773e7b82da115785b53a245
DANIEL ARZUAGA <biznobo@gmail.com>
2015-04-07 03:18:56 -0400

    implementing cache fetch on available

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -30,9 +30,9 @@ class SitesController < ApplicationController
   end
 
   def available
-    @crawl = Crawl.using(params["processor_name"]).find(params[:id])
-    @available = Page.using(params["processor_name"]).where(crawl_id: @crawl.id, available: 'true')
-    # @available = Rails.cache.read(['crawl/392/available/processor_two'])
+    # @crawl = Crawl.using(params["processor_name"]).find(params[:id])
+    # @available = Page.using(params["processor_name"]).where(crawl_id: @crawl.id, available: 'true')
+    @available = Rails.cache.fetch(["crawl/#{params[:id]}/available/#{params["processor_name"]}"]){Page.using(params["processor_name"]).where(crawl_id: params[:id], available: 'true')}
     
     sort = params[:sort].nil? ? 'id' : params[:sort]
     @pages = @available.order("#{sort} DESC").page(params[:page]).per_page(25)
