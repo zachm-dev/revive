b71e94b228ad0d7deed088a6731f371551d96f55
DANIEL ARZUAGA <biznobo@gmail.com>
2015-04-09 07:14:47 -0400

    t

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -58,7 +58,7 @@ class SitesController < ApplicationController
     
     respond_to do |format|
       format.csv { send_data @available.to_csv }
-      format.html { @pages = @available.order("#{sort} DESC").paginate(:page => page, :per_page => 25) }
+      format.html { @pages = @available.paginate(:page => page, :per_page => 25) }
     end
     
   end
