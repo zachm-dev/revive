7009913860a8831b30a41367e7f880a910ae72da
DANIEL ARZUAGA <biznobo@gmail.com>
2015-04-28 14:27:01 -0400

    t

--- app/models/crawl.rb
+++ app/models/crawl.rb
@@ -41,7 +41,7 @@ class Crawl < ActiveRecord::Base
     crawl = Crawl.using("#{processor_name}").find(options["crawl_id"].to_i)
     if crawl
       puts "here is the crawl to start #{crawl.id} on the processor #{crawl.processor_name}"
-      # Crawl.update(crawl.id, status: 'running')
+      crawl.update(status: 'running')
       puts "crawl total minutes are #{crawl.total_minutes.to_i}"
       crawl.setCrawlStartingVariables('total_minutes' => crawl.total_minutes.to_i)
       if crawl.crawl_type == 'url_crawl'
