4ebe99f7a7659ef8eafca34ab88719afce710f7a
DANIEL ARZUAGA <biznobo@gmail.com>
2015-04-29 14:54:25 -0400

    t

--- app/models/crawl.rb
+++ app/models/crawl.rb
@@ -363,28 +363,30 @@ class Crawl < ActiveRecord::Base
   def self.shut_down(options={})
     list_of_running_crawls = JSON.parse($redis.get('list_of_running_crawls'))
     crawl_that_is_shutting_down_obj = JSON.parse($redis.get('list_of_running_crawls')).select{|crawl| crawl['crawls'].has_value?(options['crawl_id'].to_i)}
-    app_name = crawl_that_is_shutting_down_obj[0]['name']
-    processor_name = crawl_that_is_shutting_down_obj[0]['crawls']["processor_name"]
-    updated_list_of_running_crawls = JSON.parse($redis.get('list_of_running_crawls')).reject{|crawl| crawl['name'].include?(app_name)}
+    if !crawl_that_is_shutting_down_obj.empty?
+      app_name = crawl_that_is_shutting_down_obj[0]['name']
+      processor_name = crawl_that_is_shutting_down_obj[0]['crawls']["processor_name"]
+      updated_list_of_running_crawls = JSON.parse($redis.get('list_of_running_crawls')).reject{|crawl| crawl['name'].include?(app_name)}
     
-    if !$redis.get('redis_urls').nil? && JSON.parse($redis.get('redis_urls')).has_key?(app_name)
-      redis_url = JSON.parse($redis.get('redis_urls'))[app_name]
-      new_redis_connection = Redis.new(url: redis_url)
-      new_redis_connection.del(new_redis_connection.keys)
-      $redis.set('list_of_running_crawls', updated_list_of_running_crawls.to_json)
-    else
-      heroku = HerokuPlatform.new
-      redis_url = heroku.get_env_vars_for(app_name, ['REDISCLOUD_URL'])['REDISCLOUD_URL']
-      if $redis.get('redis_urls').nil?
-        redis_urls = $redis.set('redis_urls', {"#{app_name}" => redis_url}.to_json)
+      if !$redis.get('redis_urls').nil? && JSON.parse($redis.get('redis_urls')).has_key?(app_name)
+        redis_url = JSON.parse($redis.get('redis_urls'))[app_name]
+        new_redis_connection = Redis.new(url: redis_url)
+        new_redis_connection.del(new_redis_connection.keys)
+        $redis.set('list_of_running_crawls', updated_list_of_running_crawls.to_json)
       else
-        redis_urls = JSON.parse($redis.get('redis_urls'))
-        redis_urls[app_name] = redis_url
-        $redis.set('redis_urls', redis_urls.to_json)
+        heroku = HerokuPlatform.new
+        redis_url = heroku.get_env_vars_for(app_name, ['REDISCLOUD_URL'])['REDISCLOUD_URL']
+        if $redis.get('redis_urls').nil?
+          redis_urls = $redis.set('redis_urls', {"#{app_name}" => redis_url}.to_json)
+        else
+          redis_urls = JSON.parse($redis.get('redis_urls'))
+          redis_urls[app_name] = redis_url
+          $redis.set('redis_urls', redis_urls.to_json)
+        end
+        new_redis_connection = Redis.new(url: redis_url)
+        new_redis_connection.del(new_redis_connection.keys)
+        $redis.set('list_of_running_crawls', updated_list_of_running_crawls.to_json)
       end
-      new_redis_connection = Redis.new(url: redis_url)
-      new_redis_connection.del(new_redis_connection.keys)
-      $redis.set('list_of_running_crawls', updated_list_of_running_crawls.to_json)
     end
     
     crawl = Crawl.using("#{processor_name}").where(id: options['crawl_id'].to_i).first
