214a22af01b80314967f4ac21eb72104096cbb85
David Lynam <dlynam@gmail.com>
2015-05-11 11:42:51 -0700

    merge admin

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -10,6 +10,7 @@
 #  created_at            :datetime         not null
 #  updated_at            :datetime         not null
 #  plan_name             :string
+#  trial_end             :string
 #
 # Indexes
 #
