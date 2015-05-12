97d7d100cb73368ed2e117159881880e5e53f294
David Lynam <dlynam@gmail.com>
2015-05-11 17:34:17 -0700

    use page_info object to extract page id and processor in delete domains service object

--- app/services/delete_domains.rb
+++ app/services/delete_domains.rb
@@ -1,16 +1,28 @@
 class DeleteDomains
-  attr_reader :page_ids, :user
+  attr_reader :page_infos, :user
 
   def initialize(params, user)
-    @page_ids = params[:page_ids]
+    @page_infos = params[:page_infos]
     @user = user
   end
 
   def go
-    page_ids.each do |page_id|
-      page = Page.find page_id
+    page_infos.each do |page_info|
+      page_info_obj = PageInfo.new(page_info)
+      page = Page.using(page_info_obj.processor).find page_info_obj.id
+      raise page.inspect
       crawl = Crawl.find_by page_id: page.id, user_id: user.id
       raise crawl.inspect
+    end
+  end
+
+  class PageInfo
+    attr_reader :id, :processor
+
+    def initialize(info)
+      info_array = info.split(',')
+      @id = info_array[0]
+      @processor = info_array[1]
     end
   end
 end
