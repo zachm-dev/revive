0bd9757d53de17ba9593853d02192d9e9df23561
David Lynam <dlynam@gmail.com>
2015-05-13 12:45:01 -0700

    update nav

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,9 +5,10 @@ class DashboardController < ApplicationController
     if params['reactivate'] == 'true'
       redirect_to reactivate_path
     else
+      @nav = 'dashboard'
       @dashboard = current_user.user_dashboard
       @domains = Page.find(@dashboard.top_domains)
     end
-
   end
+
 end
