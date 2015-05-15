0e5201bb44af06601f2706ee526eda998818f356
David Lynam <dlynam@gmail.com>
2015-05-15 10:12:39 -0700

    add nav highlite for admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,6 +2,7 @@ class AdminsController < ApplicationController
   before_filter :is_admin?
 
   def index
+    @nav = 'admin'
     @users = User.admin_search(params[:query]).order('created_at DESC').page(params[:page]).per_page(25)
   end
 
