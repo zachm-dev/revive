e896f8694dfb96f0fe8202abb8a33bbf3f1abfd9
DANIEL ARZUAGA <biznobo@gmail.com>
2015-05-19 09:11:22 -0400

    t

--- app/models/crawl.rb
+++ app/models/crawl.rb
@@ -677,21 +677,24 @@ class Crawl < ActiveRecord::Base
   
   def self.shut_down(options={})
     puts "starting to shut down crawl #{options['crawl_id']}"
-    puts "getting the crawl stats"
-    stats = Crawl.get_stats(options['crawl_id'].to_i, sender='processor')
-    puts "the crawl stats are #{stats}"
-    puts "deleting from crawler list of running crawls"
-    Crawl.remove_from_crawler_list_of_running(options['crawl_id'].to_i)
-    puts "updating status to finish"
-    Crawl.update_status_to_finish(options['crawl_id'].to_i, options['processor_name'])
-    puts "deleting from list of running crawls"
-    Crawl.remove_from_list_of_running(options['crawl_id'].to_i)
-    puts "updating crawl stats"
-    Crawl.using("#{options['processor_name']}").update(options['crawl_id'].to_i, total_urls_found: stats['total_urls_found'].to_i, total_broken: stats['total_broken'].to_i, total_expired: stats['total_expired'].to_i)
-    puts "deleting redis keys"
-    Crawl.delete_redis_keys_for(options['crawl_id'].to_i, 'processor')
-    puts "shut down crawl successfully #{options['crawl_id']}"
-
+    
+    crawl = Crawl.using("#{options['processor_name']}").where(id: options['crawl_id'].to_i).first
+    if crawl && crawl.status != 'finished'
+      puts "getting the crawl stats"
+      stats = Crawl.get_stats(options['crawl_id'].to_i, sender='processor')
+      puts "the crawl stats are #{stats}"
+      puts "deleting from crawler list of running crawls"
+      Crawl.remove_from_crawler_list_of_running(options['crawl_id'].to_i)
+      puts "updating status to finish"
+      Crawl.update_status_to_finish(options['crawl_id'].to_i, options['processor_name'])
+      puts "deleting from list of running crawls"
+      Crawl.remove_from_list_of_running(options['crawl_id'].to_i)
+      puts "updating crawl stats"
+      Crawl.using("#{options['processor_name']}").update(options['crawl_id'].to_i, total_urls_found: stats['total_urls_found'].to_i, total_broken: stats['total_broken'].to_i, total_expired: stats['total_expired'].to_i)
+      puts "deleting redis keys"
+      Crawl.delete_redis_keys_for(options['crawl_id'].to_i, 'processor')
+      puts "shut down crawl successfully #{options['crawl_id']}"
+    end
     # list_of_running_crawls = JSON.parse($redis.get('list_of_running_crawls'))
     # crawl_to_shut_down = list_of_running_crawls.select{|crawl| crawl['crawl_id'].to_i == options['crawl_id'].to_i}
     # updated_list_of_running_crawls = list_of_running_crawls.reject{|crawl| crawl['crawl_id'].to_i == options['crawl_id'].to_i}
