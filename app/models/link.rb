ade4cc1f94f09831cf80d0d702c6b5ba8137d8e5
DANIEL ARZUAGA <biznobo@gmail.com>
2015-05-22 03:38:12 -0400

    t

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,73 +8,75 @@ class Link < ActiveRecord::Base
   def self.start_processing(options={})
 
     puts "start_processing: start of method"
-    
     stats = Sidekiq::Stats.new.queues["process_links"].to_i
     
     if stats < 500
-
       running_crawls = Rails.cache.read(['running_crawls']).to_a
       puts "start_processing: list of running crawls #{running_crawls}"
       if !running_crawls.empty?
-        next_crawl_to_process = running_crawls[0]
-        puts "next crawl to process #{next_crawl_to_process}"
-        processing_link_ids = Rails.cache.read(["crawl/#{next_crawl_to_process}/processing_batches/ids"]).to_a
+        if Rails.cache.read(["crawl/#{running_crawls[0]}/expired_ids"]).to_a.count < 5
+          next_crawl_to_process = running_crawls[0]
+          puts "next crawl to process #{next_crawl_to_process}"
+          processing_link_ids = Rails.cache.read(["crawl/#{next_crawl_to_process}/processing_batches/ids"]).to_a
 
-        if !processing_link_ids.empty?
-          puts "start_processing: there are more links to be processed"
-          next_link_id_to_process = processing_link_ids[0]
-          puts "the next link to be processed is #{next_link_id_to_process}"
-          new_crawls_rotation = running_crawls.rotate
-          
-          puts "deleting process link id from batch for crawl #{options['crawl_id']}"
-          Rails.cache.write(["crawl/#{next_crawl_to_process}/processing_batches/ids"], processing_link_ids-[next_link_id_to_process])
-          
-          redis_obj = JSON.parse($redis.get(next_link_id_to_process))
-          puts "start_processing: the redis obj is #{redis_obj}"
-        
-          Rails.cache.write(['running_crawls'], new_crawls_rotation)
-          Rails.cache.write(['current_processing_batch_id'], "#{next_link_id_to_process}")
-            
-          processor_name = redis_obj['processor_name']
-          site_id = redis_obj['site_id'].to_i
-          crawl_id = redis_obj['crawl_id'].to_i
-          base_url = redis_obj['base_url']
-          crawl = Crawl.using("#{processor_name}").where(id: crawl_id).first
-          domain = Domainatrix.parse(base_url).domain
-
-          Rails.cache.increment(["crawl/#{crawl_id}/processing_batches/total"])
-          Rails.cache.increment(["crawl/#{crawl_id}/processing_batches/running"])
-
-          if Rails.cache.read(["site/#{site_id}/processing_batches/total"], raw: true).to_i == 0
-            site = Site.using("#{processor_name}").where(id: site_id).first
-            puts "updating site and creating new starting variables for processing batch for the site #{site_id}"
-            site.update(processing_status: 'running')
-            Rails.cache.write(["site/#{site_id}/processing_batches/total"], 1, raw: true)
-            Rails.cache.write(["site/#{site_id}/processing_batches/running"], 1, raw: true)
-            Rails.cache.write(["site/#{site_id}/processing_batches/finished"], 0, raw: true)
-          else
-            puts 'incrementing process batch stats'
-            Rails.cache.increment(["site/#{site_id}/processing_batches/total"])
-            Rails.cache.increment(["site/#{site_id}/processing_batches/running"])
-          end
-    
-          puts " process links on complete variables link id #{next_link_id_to_process} site id #{site_id} and crawl id #{crawl_id}"
-    
-          batch = Sidekiq::Batch.new
-          batch.on(:complete, ProcessLinks, 'bid' => batch.bid, 'crawl_id' => crawl_id, 'site_id' => site_id, 'redis_id' => next_link_id_to_process, 'user_id' => crawl.user_id, 'crawl_type' => crawl.crawl_type, 'iteration' => crawl.iteration.to_i, 'processor_name' => processor_name)
-          
-          batch.jobs do
-            redis_obj['links'].each{|l| ProcessLinks.perform_async(l, site_id, redis_obj['found_on'], domain, crawl_id, 'processor_name' => processor_name)}
-          end
-
-        else
-          if running_crawls.count > 1
+          if !processing_link_ids.empty?
+            puts "start_processing: there are more links to be processed"
+            next_link_id_to_process = processing_link_ids[0]
+            puts "the next link to be processed is #{next_link_id_to_process}"
             new_crawls_rotation = running_crawls.rotate
+          
+            puts "deleting process link id from batch for crawl #{options['crawl_id']}"
+            Rails.cache.write(["crawl/#{next_crawl_to_process}/processing_batches/ids"], processing_link_ids-[next_link_id_to_process])
+          
+            redis_obj = JSON.parse($redis.get(next_link_id_to_process))
+            puts "start_processing: the redis obj is #{redis_obj}"
+        
             Rails.cache.write(['running_crawls'], new_crawls_rotation)
-            Link.delay.start_processing
-          end 
-        end
+            Rails.cache.write(['current_processing_batch_id'], "#{next_link_id_to_process}")
+            
+            processor_name = redis_obj['processor_name']
+            site_id = redis_obj['site_id'].to_i
+            crawl_id = redis_obj['crawl_id'].to_i
+            base_url = redis_obj['base_url']
+            crawl = Crawl.using("#{processor_name}").where(id: crawl_id).first
+            domain = Domainatrix.parse(base_url).domain
 
+            Rails.cache.increment(["crawl/#{crawl_id}/processing_batches/total"])
+            Rails.cache.increment(["crawl/#{crawl_id}/processing_batches/running"])
+
+            if Rails.cache.read(["site/#{site_id}/processing_batches/total"], raw: true).to_i == 0
+              site = Site.using("#{processor_name}").where(id: site_id).first
+              puts "updating site and creating new starting variables for processing batch for the site #{site_id}"
+              site.update(processing_status: 'running')
+              Rails.cache.write(["site/#{site_id}/processing_batches/total"], 1, raw: true)
+              Rails.cache.write(["site/#{site_id}/processing_batches/running"], 1, raw: true)
+              Rails.cache.write(["site/#{site_id}/processing_batches/finished"], 0, raw: true)
+            else
+              puts 'incrementing process batch stats'
+              Rails.cache.increment(["site/#{site_id}/processing_batches/total"])
+              Rails.cache.increment(["site/#{site_id}/processing_batches/running"])
+            end
+    
+            puts " process links on complete variables link id #{next_link_id_to_process} site id #{site_id} and crawl id #{crawl_id}"
+    
+            batch = Sidekiq::Batch.new
+            batch.on(:complete, ProcessLinks, 'bid' => batch.bid, 'crawl_id' => crawl_id, 'site_id' => site_id, 'redis_id' => next_link_id_to_process, 'user_id' => crawl.user_id, 'crawl_type' => crawl.crawl_type, 'iteration' => crawl.iteration.to_i, 'processor_name' => processor_name)
+          
+            batch.jobs do
+              redis_obj['links'].each{|l| ProcessLinks.perform_async(l, site_id, redis_obj['found_on'], domain, crawl_id, 'processor_name' => processor_name)}
+            end
+
+          else
+            if running_crawls.count > 1
+              new_crawls_rotation = running_crawls.rotate
+              Rails.cache.write(['running_crawls'], new_crawls_rotation)
+              Link.delay.start_processing
+            end 
+          end
+        else
+          puts "routing from process links to verify domain"
+          VerifyNamecheap.delay(:queue => 'verify_domains').start
+        end
       end
     end
     
